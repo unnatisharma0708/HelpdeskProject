78178cff5544593be66e78e82b802018d295f326
unnati sharma <unnatisharma0708@gmail.com>
2020-03-02 19:25:20 +0530

    first commit

---
+++ src/test/java/com/over2cloud/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.over2cloud.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
